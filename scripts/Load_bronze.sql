3ff872dda48a646027ed236f391fb6cbe258567a
Honey jha <surveshjha20@gmail.com>
2025-05-23 14:37:18 +0530

    Added Comments in Stored Procedure

--- scripts/Load_bronze.sql
+++ scripts/Load_bronze.sql
@@ -6,8 +6,12 @@ AS $$
 BEGIN
     RAISE NOTICE 'Starting data load for bronze layer...';
 
+	RAISE NOTICE '=======================================';
+	RAISE NOTICE 'LOAD SOURCE CRM';
+	RAISE NOTICE '=======================================';
     -- Load crm_prd_info
     TRUNCATE TABLE bronze.crm_prd_info;
+	RAISE NOTICE 'TRUNCATE TABLE bronze.crm_prd_info';
     COPY bronze.crm_prd_info (prd_id, prd_key, prd_nm, prd_cost, prd_line, prd_start_dt, prd_end_dt)
     FROM 'E:/DataEngineering/Git/sql-datawarehouse-project/datasets/source_crm/prd_info.csv'
     DELIMITER ','
@@ -17,6 +21,7 @@ BEGIN
 
     -- Load crm_cust_info
     TRUNCATE TABLE bronze.crm_cust_info;
+	RAISE NOTICE 'TRUNCATE TABLE bronze.crm_cust_info';
     COPY bronze.crm_cust_info (cst_id,cst_key,cst_firstname,cst_lastname,cst_marital_status,cst_gndr,cst_create_date)
     FROM 'E:/DataEngineering/Git/sql-datawarehouse-project/datasets/source_crm/cust_info.csv'
     DELIMITER ','
@@ -26,15 +31,21 @@ BEGIN
 
     -- Load crm_sales_details
     TRUNCATE TABLE bronze.crm_sales_details;
+	RAISE NOTICE 'TRUNCATE TABLE bronze.crm_sales_details';
     COPY bronze.crm_sales_details (sls_ord_num,sls_prd_key,sls_cust_id,sls_order_d,sls_ship_dt,sls_due_dt,sls_sales,sls_quantit,sls_price)
     FROM 'E:/DataEngineering/Git/sql-datawarehouse-project/datasets/source_crm/sales_details.csv'
     DELIMITER ','
     CSV
     HEADER;
     RAISE NOTICE 'Loaded bronze.crm_sales_details';
+	
+	RAISE NOTICE '=======================================';
+	RAISE NOTICE 'LOAD SOURCE ERP';
+	RAISE NOTICE '=======================================';
 
     -- Load erp_cat_g1v2
     TRUNCATE TABLE bronze.erp_cat_g1v2;
+	RAISE NOTICE 'TRUNCATE TABLE bronze.erp_cat_g1v2';
     COPY bronze.erp_cat_g1v2 (ID,CAT,SUBCAT,MAINTENANCE)
     FROM 'E:/DataEngineering/Git/sql-datawarehouse-project/datasets/source_erp/PX_CAT_G1V2.csv'
     DELIMITER ','
@@ -44,6 +55,7 @@ BEGIN
 
     -- Load erp_cust_az12
     TRUNCATE TABLE bronze.erp_cust_az12;
+	RAISE NOTICE 'TRUNCATE TABLE bronze.erp_cust_az12';
     COPY bronze.erp_cust_az12 (CID,BDATE,GEN)
     FROM 'E:/DataEngineering/Git/sql-datawarehouse-project/datasets/source_erp/CUST_AZ12.csv'
     DELIMITER ','
@@ -53,6 +65,7 @@ BEGIN
 
     -- Load erp_loc_a101
     TRUNCATE TABLE bronze.erp_loc_a101;
+	RAISE NOTICE 'TRUNCATE TABLE bronze.erp_loc_a101';
     COPY bronze.erp_loc_a101(CID,CNTRY)
     FROM 'E:/DataEngineering/Git/sql-datawarehouse-project/datasets/source_erp/LOC_A101.csv'
     DELIMITER ','
